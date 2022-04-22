8a6d8bed4e1b4f5eaf56c36172203d4398a2c8e9
Ivan Uresti <ivan.uresti@gmail.com>
2022-04-22 09:10:55 -0500

    Fix mockito version

--- tpv-app/src/main/java/oop/inheritance/Main.java
+++ tpv-app/src/main/java/oop/inheritance/Main.java
@@ -8,18 +8,29 @@ public class Main {
         Application application = new Application(SupportedTerminal.INGENICO);
 
         while (true) {
+            run(application);
+        }
+    }
 
-            application.clearScreen();
-            application.showMenu();
+    public static void run(Application application) {
+        application.clearScreen();
+        application.showMenu();
 
-            String key = application.readKey();
+        String key = application.readKey();
 
-            switch (key) {
-                case "1" -> application.doSale();
-                case "2" -> application.doRefund();
-                case "3" -> application.printReport();
-                case "4" -> application.showConfiguration();
-            }
+        switch (key) {
+            case "1":
+                application.doSale();
+                break;
+            case "2":
+                application.doRefund();
+                break;
+            case "3":
+                application.printReport();
+                break;
+            case "4":
+                application.showConfiguration();
+                break;
         }
     }
 
